d7c07925135374b56e669c3a5f918ac3552b90ed
revzankamarullah <revzankamarullah@gmail.com>
2017-05-14 02:01:49 +0700

    add remove todo feature

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Todo from './todo.js';
 
-const TodoList = ({todos, complete, completed}) => {
-    let todoList = todos.map((todo, index) => <Todo index={index} completed={todo.completed} complete={complete} key={index}>{todo.title}</Todo>);
+const TodoList = ({todos, complete, completed, remove}) => {
+    let todoList = todos.map((todo, index) => <Todo index={index} remove={remove} completed={todo.completed} complete={complete} key={index}>{todo.title}</Todo>);
     return (<ul>{todoList}</ul>)
 };
 
--- src/components/todo.js
+++ src/components/todo.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 const isCompleted = (param) => param === true ? 'completed' : '';
 
-const Todo = ({children, index, complete, completed}) => <li className={isCompleted(completed)}><a><input checked={completed} onChange={complete} className="checkbox" type="checkbox" id={index}/> {children}</a></li>;
+const Todo = ({children, index, complete, completed, remove}) => <li className={isCompleted(completed)}><a><input checked={completed} onChange={complete} className="checkbox" type="checkbox" id={index}/> {children} </a><span className="close" onClick={remove} id={index}>Remove</span></li>;
 
 export default Todo;
